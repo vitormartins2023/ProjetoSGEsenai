d97931bda4223a7f610c09baee7052bc5113f448
vitormartins2023 <vitor.rodofort@gmail.com>
2024-04-11 21:53:00 -0300

    front

--- backend/sge/settings.py
+++ backend/sge/settings.py
@@ -54,6 +54,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,6 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ frontend/sge-front/pages/home/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const { signOut } = useAuth();
+
+definePageMeta({
+    middleware: 'auth'
+});
+
+const logout = ()=> {
+    signOut({redirect: false})
+    .then(()=>{
+        console.log("successfully logged out");
+        navigateTo("/");
+    })
+    .catch((error)=>{
+        console.log("Error when trying to execute logout: ", error);
+    });
+}
+
+
+</script>
+
+<template>
+    <div>
+        <h1>Minha Tela Home!!!</h1>   
+        <button @click="logout" class="customButton">SAIR</button> 
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
